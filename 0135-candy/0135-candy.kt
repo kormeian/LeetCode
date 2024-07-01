d03fe2b6a3cc4ff53763fd829ba53093a8cb5908
Ian Kim <kormeian@gmail.com>
2024-07-01 16:49:16 +0900

    Time: 253 ms (32.64%), Space: 40.3 MB (32.12%) - LeetHub

---
+++ 0135-candy/0135-candy.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun candy(ratings: IntArray): Int {
+        val n = ratings.size
+        val candies = IntArray(n) { 1 }
+
+        for (i in 1 until n) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1
+            }
+        }
+
+        for (i in n - 2 downTo 0) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = maxOf(candies[i], candies[i + 1] + 1)
+            }
+        }
+
+        return candies.sum()
+    }
+}
